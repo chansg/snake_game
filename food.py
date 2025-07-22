7973254d1060ee715ea26e9ebb92b703392ab437
chansg <chansg@protonmail.com>
2025-07-22 19:45:10 +0100

    Initial commit

---
+++ food.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+
+
+    def __init__(self):
+        super().__init__()
+        # We're only able to use these Turtle methods because
+        # we're inheriting from the Super class
+        self.shape("circle")
+        self.color("blue")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.speed("fastest")
+        self.refresh()
+
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
